0c7c259e8d1d7256cb8badb341044c4401f9bf16
r351574nc3 <r351574nc3@gmail.com>
2015-08-08 21:10:25 -0700

    Setting up initial project and web-based registration/authentication

---
+++ src/main/java/com/github/r351574nc3/firejackr/App.java
@@ -0,0 +1,13 @@
+package com.github.r351574nc3.firejackr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
